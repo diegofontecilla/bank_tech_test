5c44a2ea8721ac4b67934a858350410deedb919f
diego fontecilla <mail@diegofontecilla.com>
2018-07-16 11:45:52 +0100

    User can make a deposit on its account

--- lib/bank_app.rb
+++ lib/bank_app.rb
@@ -1,1 +1,11 @@
+class BankApp
+  attr_reader :account
 
+  def initialize
+    @account = 0
+  end
+
+  def deposit(amount)
+    @account += amount
+  end
+end
--- spec/bank_app_spec.rb
+++ spec/bank_app_spec.rb
@@ -1,1 +1,9 @@
+require 'bank_app'
 
+describe BankApp do
+  it 'user can make a deposit on its account' do
+    bank_app = BankApp.new
+    bank_app.deposit(20)
+    expect(bank_app.account).to eq(20)
+  end
+end
