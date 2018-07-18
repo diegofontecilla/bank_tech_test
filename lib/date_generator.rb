e61da5175df656781035f9abbe560156f25df72c
diego fontecilla <mail@diegofontecilla.com>
2018-07-18 11:24:14 +0100

    Added  print_date test and method for DateGenerator class

--- lib/date_generator.rb
+++ lib/date_generator.rb
@@ -1,6 +1,13 @@
-require 'date'
+require 'time'
 
 class DateGenerator
+
+  def print_date
+    get_time
+    @current_time.strftime "%d/%m/%Y %H:%M"
+  end
+
+# I want to make get_time private but had trouble testing it
 
   def get_time
     @current_time = Time.now
--- spec/date_generator_spec.rb
+++ spec/date_generator_spec.rb
@@ -9,4 +9,12 @@ describe DateGenerator do
       expect(date_generator.get_time).to eq("2018-07-17 14:41:01")
     end
   end
+
+  context '#print_date' do
+    it 'prints date and time' do
+      date_generator = DateGenerator.new
+      allow(Time).to receive(:now).and_return(Time.parse("18/07/2018 10:14"))
+      expect(date_generator.print_date).to eq("18/07/2018 10:14")
+    end
+  end
 end
